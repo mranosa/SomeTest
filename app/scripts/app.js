cc41f8451361268d1dcff54ee5f51e5147394b36
Mark Kenneth Ranosa <Ken@Marks-MacBook-Pro.local>
2014-05-12 15:30:45 +0800

    Some initial working stuffs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,12 @@ angular.module('someTestApp', [
   'ngSanitize',
   'ngRoute'
 ])
-  .config(function ($routeProvider) {
+  .config(function($routeProvider) {
+    Date.prototype.addDays = function(days) {
+      this.setDate(this.getDate() + days);
+      return this;
+    };
+    
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,35 @@
 'use strict';
 
 angular.module('someTestApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('MainCtrl', function($scope) {
+		$scope.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+
+		$scope.getSunday = function() {
+			var currentDay = new Date(),
+				sundayDate = currentDay.getDate() - currentDay.getDay(),
+				tempDate = new Date();
+
+			tempDate.setDate(sundayDate);
+
+			return new Date(tempDate);
+		};
+
+		$scope.getDatesOfTheWeek = function(sunday) {
+			var startDate = sunday,
+				dates = [
+					new Date(startDate)
+				];
+
+			for(var i = 0; i < 6; i++) {
+				dates.push(new Date(startDate.addDays(1)));
+			}
+
+			return dates;
+		};
+
+		$scope.datesOfTheWeek = $scope.getDatesOfTheWeek($scope.getSunday());
+	});
